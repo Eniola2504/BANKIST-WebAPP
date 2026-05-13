466a686ab1a8ab3626028a0c8cc0db67ab4ac9d3
Enyholla <sannifawaz2504@gmail.com>
2026-05-14 00:37:55 +0100

    add lazy load functionality using intersection API observer

--- script.js
+++ script.js
@@ -140,3 +140,28 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// lazy load image
+
+const allImage = document.querySelectorAll('img.features__img');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+allImage.forEach(img => imgObserver.observe(img));
